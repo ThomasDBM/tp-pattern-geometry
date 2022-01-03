5a6e5d46370601c932849e3dc1ccb0c82e66ea41
thomasDBM <thomasdebeaumont63@gmail.com>
2022-01-03 14:11:04 +0100

    implementCoord

--- src/main/java/org/acme/geometry/Coordinate.java
+++ src/main/java/org/acme/geometry/Coordinate.java
@@ -1,5 +1,25 @@
 package org.acme.geometry;
 
 public class Coordinate {
+    
+    private double x;
+    private double y;
 
+    Coordinate(){
+        this.x = 0.0;
+        this.y = 0.0;
+    }
+
+    Coordinate(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    double getX(){
+        return this.x;
+    }
+
+    double getY(){
+        return this.y;
+    }
 }
