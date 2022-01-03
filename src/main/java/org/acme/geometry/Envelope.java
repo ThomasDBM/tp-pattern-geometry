73caf6317b6fa1e645fd40bf75029842ce3de520
thomasDBM <thomasdebeaumont63@gmail.com>
2022-01-03 16:59:18 +0100

    create files for envelope

---
+++ src/main/java/org/acme/geometry/Envelope.java
@@ -0,0 +1,13 @@
+package org.acme.geometry;
+
+public class Envelope {
+
+    private Coordinate bottomLeft;
+    private Coordinate topRight;
+
+    Envelope(){
+        this.bottomLeft = new Coordinate();
+        this.topRight = new Coordinate();
+    }
+    
+}
